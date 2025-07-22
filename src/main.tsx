9ad45cb58b40c0728c142896b9ff9f3d4efa631f
Kandiyas35 <ibnukandiyas35@gmail.com>
2025-07-22 15:37:14 +0700

    Update: perubahan terbaru

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+// Import Vercel Speed Insights
+import { injectSpeedInsights } from "@vercel/speed-insights";
+
+// Inisialisasi Speed Insights
+injectSpeedInsights();
 
 createRoot(document.getElementById("root")!).render(<App />);
+s;
